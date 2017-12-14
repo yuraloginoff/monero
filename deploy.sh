f9df28b42f446d1db22073d24d4baac9d93d23c1
Yura Loginov <yuraloginoff@gmail.com>
2017-12-14 17:15:08 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ wget https://minergate.com/download/deb-cli
 mv deb-cli minergate-cli-release.deb
 sudo apt install gdebi
 sudo gdebi minergate-cli-release.deb
+
+minergate-cli -user yuraloginoff@gmail.com -xmr
